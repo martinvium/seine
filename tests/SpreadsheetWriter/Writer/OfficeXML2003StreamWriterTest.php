67e2ab41d9f24ef3f4cc409ca669a178cb9eeb70
Martin Vium <martin.vium@gmail.com>
2011-09-10 18:10:26 +0200

    Reorganize office 2003 test

--- tests/SpreadsheetWriter/Writer/OfficeXML2003StreamWriterTest.php
+++ tests/SpreadsheetWriter/Writer/OfficeXML2003StreamWriterTest.php
@@ -1,24 +1,33 @@
 <?php
-namespace SpreadSheetWriter;
+namespace SpreadSheetWriter\Writer;
 
-require_once(dirname(__DIR__) . '/bootstrap.php');
+require_once(dirname(dirname(__DIR__)) . '/bootstrap.php');
 
 use SpreadSheetWriter\Parser\DOM\DOMFactory;
 
-class ExampleTest extends \PHPUnit_Framework_TestCase
+class OfficeXML2003StreamWriterTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var DOMFactory
+     */
+    private $factory;
+    
+    public function setUp()
+    {
+        parent::setUp();
+        $this->factory = new DOMFactory();
+    }
+    
     public function testMultipleSheetsWithStyles()
     {
-        $actual_file = __DIR__ . '/_files/out.xml';
+        $actual_file = __DIR__ . '/_files/actual_valid.xml';
         
-        $factory = new DOMFactory();
-        $book = $factory->getBook();
-        $fp = fopen($actual_file, 'w');
-        $book->setWriter($factory->getWriterFactory()->getOfficeXML2003StreamWriter($fp));
-
+        $fp = $this->makeStream($actual_file);
+        
+        $book = $this->makeBookWithWriter($fp);
         $sheet = $book->addSheetByName('more1');
         for($i = 0; $i < 100; $i++) {
-            $sheet->addRow($factory->getRow(array(
+            $sheet->addRow($this->factory->getRow(array(
                 'cell1',
                 'cell2',
                 'cell3',
@@ -28,9 +37,9 @@ class ExampleTest extends \PHPUnit_Framework_TestCase
 
         $sheet = $book->addSheetByName('more2');
         $style = $sheet->addStyleById('mystyle')->setFontBold(true);
-        $sheet->addRow($factory->getRow(array('head1', 'head2', 'head3', 'head4'))->setStyle($style));
+        $sheet->addRow($this->factory->getRow(array('head1', 'head2', 'head3', 'head4'))->setStyle($style));
         for($i = 0; $i < 1000; $i++) {
-            $sheet->addRow($factory->getRow(array(
+            $sheet->addRow($this->factory->getRow(array(
                 'cell1',
                 'cell2',
                 rand(100, 10000),
@@ -54,20 +63,32 @@ class ExampleTest extends \PHPUnit_Framework_TestCase
         $num_columns = 100;
         
         $start_timestamp = microtime(true);
-        $actual_file = __DIR__ . '/_files/out_perf.xml';
-        $factory = new DOMFactory();
-        $book = $factory->getBook();
+        $actual_file = __DIR__ . '/_files/performance.xml';
         
-        $fp = fopen($actual_file, 'w');
-        $book->setWriter($factory->getWriterFactory()->getOfficeXML2003StreamWriter($fp));
+        $fp = $this->makeStream($actual_file);
+        
+        $book = $this->makeBookWithWriter($fp);
         $sheet = $book->addSheetByName('more1');
         for($i = 0; $i < $num_rows; $i++) {
-            $sheet->addRow($factory->getRow(range(0, $num_columns)));
+            $sheet->addRow($this->factory->getRow(range(0, $num_columns)));
         }
         $book->close();
         fclose($fp);
         
         $this->assertLessThan($memory_limit, memory_get_peak_usage(true), 'memory limit reached');
         $this->assertLessThan($time_limit_seconds, (microtime(true) - $start_timestamp), 'time limit reached');
+    }
+    
+    private function makeStream($filename)
+    {
+        return fopen($filename, 'w');
+    }
+    
+    private function makeBookWithWriter($fp)
+    {
+        $book = $this->factory->getBook();
+        $writer = $this->factory->getWriterFactory()->getOfficeXML2003StreamWriter($fp);
+        $book->setWriter($writer);
+        return $book;
     }
 }
