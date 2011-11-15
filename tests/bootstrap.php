0b8a849cab85b77d1ab9e3006b57655c57493d1c
Martin Vium <martin.vium@gmail.com>
2011-11-15 21:14:05 +0100

    Update test bootstrap and travil config to utilize composer for autoloading

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,10 +1,2 @@
 <?php
-$src_path = dirname(__DIR__) . '/src';
-$tests_path = __DIR__;
-set_include_path(get_include_path() . PATH_SEPARATOR . $src_path . PATH_SEPARATOR . $tests_path);
-
-spl_autoload_register(function($class) {
-    return spl_autoload(str_replace('\\', '/', $class));
-});
-
-require_once 'PHPUnit/Framework/TestCase.php';
+require_once(dirname(__DIR__) . '/vendor/.composer/autoload.php');
